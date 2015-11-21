66aa8d1d32d91e4be331401781553fa76ae7a68c
Johan Bergström <johan.carl.bergstrom@gmail.com>
2015-11-21 18:26:04 +0100

    Document

--- src/main/java/com/devexed/dbsource/jdbc/JdbcAccessorFactory.java
+++ src/main/java/com/devexed/dbsource/jdbc/JdbcAccessorFactory.java
@@ -1,5 +1,8 @@
 package com.devexed.dbsource.jdbc;
 
+/**
+ * A factory which creates JDBC column and parameter accessors for types.
+ */
 public interface JdbcAccessorFactory {
 
     JdbcAccessor create(Class<?> type);
